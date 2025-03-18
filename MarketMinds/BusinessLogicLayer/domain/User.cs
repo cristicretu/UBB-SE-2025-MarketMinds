1cbdebc8dd544dc15205656e4dd6f09f5bf11a7b
Ghestr <malosalexandru2004@gmail.com>
2025-03-18 19:31:49 +0200

    Created the User and Product
    Product is empty, just need it for the Reviews

---
+++ MarketMinds/BusinessLogicLayer/domain/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.domain
+{
+    internal class User
+    {
+        internal int _id { get; set; }
+        internal string _username { get; set; } = string.Empty;
+        internal string _email { get; set; } = string.Empty;
+        internal string _userType { get; set; } = string.Empty;
+        internal float _balance { get; set; }
+        internal float _rating { get; set; }
+        internal float _password { get; set; }
+
+
+    }
+}
