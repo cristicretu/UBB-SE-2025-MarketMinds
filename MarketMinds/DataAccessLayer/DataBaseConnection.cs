1a9ea385f30b6c26ad6c1f994b4e0a24e1d26d89
Ghestr <malosalexandru2004@gmail.com>
2025-03-18 20:42:37 +0200

    Added the connection table, and the Review Table

---
+++ MarketMinds/DataAccessLayer/DataBaseConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetEnv;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    internal class DataBaseConnection
+    {
+        SqlConnection sqlConnection = null;
+
+        static void main(string[] args)
+        {
+            Env.Load();
+
+            //DATABASE_URL=Server=servername;Database=MarketPlace;User Id=user;Password=password
+            string connectionString = Env.GetString("DATABASE_URL");
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    Console.WriteLine("Connected");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
