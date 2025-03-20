59cef026949887134c0b29450291fc7c0445f29a
Ghestr <malosalexandru2004@gmail.com>
2025-03-20 17:20:38 +0200

    Starting commit
    Started working on my branch again, some things need commited for some reason

--- MarketMinds/DataAccessLayer/DataBaseConnection.cs
+++ MarketMinds/DataAccessLayer/DataBaseConnection.cs
@@ -13,21 +13,10 @@ namespace DataAccessLayer
     {
 
         SqlConnection sqlConnection;
-        private string connectionString;
+        private string connectionString = "Server=GHASTERLAPTOP;Database=MarketPlace;User Id=sa;Password=1808;TrustServerCertificate=True";
 
         public DataBaseConnection()
-        {
-
-            string envPath = Path.Combine(AppContext.BaseDirectory, "login.env");
-            Env.Load(envPath);
-
-
-            //DATABASE_URL=Server=servername;Database=MarketPlace;User Id=user;Password=password;TrustServerCertificate=True
-            this.connectionString = Env.GetString("DATABASE_URL");
-            Console.WriteLine($"Current Directory: {Directory.GetCurrentDirectory()}");
-            Debug.WriteLine($"DATABASE_URL: {Env.GetString("DATABASE_URL")}");
-            Console.WriteLine(connectionString);
-            
+        {   
             this.sqlConnection = new SqlConnection(connectionString);
         }
 
