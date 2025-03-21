bdc45f0b93f80c5a7c0035ed4b361cbf872c0665
Ghestr <malosalexandru2004@gmail.com>
2025-03-21 21:42:41 +0200

    Done with the services part

---
+++ MarketMinds/BusinessLogicLayer/Services/ReviewsService.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Domain;
+
+namespace BusinessLogicLayer.Services
+{
+    public class ReviewsService
+    {
+        public ReviewRepository Repository;
+
+        public ReviewsService(ReviewRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public List<Review> GetReviewsBySeller(User seller)
+        {
+            List<Review> reviews = Repository.GetAllReviewsBySeller(seller);
+            // add to Review date and sort by dates
+            //reviews.Sort()
+            return reviews;
+        }
+
+        public List<Review> GetReviewsByBuyer(User buyer)
+        {
+            List<Review> reviews = Repository.GetAllReviewsByBuyer(buyer);
+            // same here
+            return reviews;
+        }
+
+        public void AddReview(string description, List<Image> images, float rating, User seller, User buyer)
+        {
+            Repository.CreateReview(new Review(-1,description, images, rating, seller.id, buyer.id));
+        }
+
+        public void EditReview(string description, List<Image> images, float rating, User seller, User buyer, string newDescription, float newRating)
+        {
+            // change in uml aswell
+            Repository.EditReview(new Review(-1, description, images, rating, seller.id, buyer.id), newRating, newDescription);
+        }
+
+        public void DeleteReview(string description, List<Image> images, float rating, User seller, User buyer)
+        {
+            Repository.DeleteReview(new Review(-1, description, images, rating, seller.id, buyer.id));
+        }
+    }
+}
--- MarketMinds/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ MarketMinds/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b82c367c15952968521652dd28abea5070b020")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d558a56fb0ad4d229e80e5f1516dae660d891b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
