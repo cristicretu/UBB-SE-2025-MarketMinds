2aa6095e8d3d717df876c875cff4f7bbddde645b
Robistyle <robi9.lel@gmail.com>
2025-03-23 14:43:24 +0200

    test

--- MarketMinds/BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
+++ MarketMinds/BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce8aef4b4376ae45d27bf0bcadc48249d9ad9ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5af595f42509458a87fced98bdf93214e835844")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
