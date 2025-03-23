dce8aef4b4376ae45d27bf0bcadc48249d9ad9ce
Robistyle <robi9.lel@gmail.com>
2025-03-23 10:09:46 +0200

    Merged with main

--- MarketMinds/BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
+++ MarketMinds/BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89af9eb0f0faa132e936ca08357df5dd35a6851b")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5748f09193b621714a1acd1c49317d3d455e64")]
->>>>>>> bidding
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537df42c44d04f25be337d63578db2dfc856f90e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
