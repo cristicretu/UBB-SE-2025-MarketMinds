6da31281a6b720e96573aa45b476ffbd656eee30
Ghestr <malosalexandru2004@gmail.com>
2025-03-25 19:19:39 +0200

    Made the ui for creating work
    it's kind of done, I cant make it work with half stars for now

--- MarketMinds/ViewModelLayer/ViewModel/ReviewCreateViewModel.cs
+++ MarketMinds/ViewModelLayer/ViewModel/ReviewCreateViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLogicLayer.Services;
 using DomainLayer.Domain;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace ViewModelLayer.ViewModel
 {
@@ -12,16 +14,39 @@ namespace ViewModelLayer.ViewModel
         public string Description { get; set; }
         public List<Image> Images { get; set; }
         public float Rating { get; set; }
+        public string ImagesString
+        {
+            get => Images != null ? string.Join("\n", Images.Select(img => img.url)) : "";
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    Images = value.Split("\n").Select(url => new Image(url)).ToList();
+                }
+                else
+                {
+                    Images = new List<Image>();
+                }
+            }
+        }
 
-        public ReviewCreateViewModel(ReviewsService reviewsService, User buyer)
+        public ReviewCreateViewModel(ReviewsService reviewsService, User buyer, User seller)
         {
             ReviewsService = reviewsService;
             Buyer = buyer;
+            Seller = seller;
         }
 
         public void SubmitReview()
         {
+            Debug.WriteLine("Submitting Review");
+            if (Seller == null || Buyer == null || ReviewsService == null)
+            {
+                Debug.WriteLine("One of the required objects is null!");
+                return;
+            }
             ReviewsService.AddReview(Description, Images, Rating, Seller, Buyer);
         }
+
     }
 }
