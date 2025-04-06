b9ff1db3adfcb1d7f0cdeebbec02cd420be77cd4
Vasile Draguta <dragutavasile@gmail.com>
2025-04-06 15:07:12 +0300

    100% for auctionrepo and baskedrepo

--- MarketMinds.Test/Repositories/AuctionProductsRepositoryTest/AuctionProductsRepositoryTest.cs
+++ MarketMinds.Test/Repositories/AuctionProductsRepositoryTest/AuctionProductsRepositoryTest.cs
@@ -9,6 +9,7 @@ using DomainLayer.Domain;
 using DataAccessLayer;
 using Microsoft.Extensions.Configuration;
 using MarketMinds.Test.Utils;
+using MarketMinds.Repositories;
 
 namespace MarketMinds.Tests.AuctionProductsRepositoryTest
 {
@@ -25,6 +26,7 @@ namespace MarketMinds.Tests.AuctionProductsRepositoryTest
         ProductCategory testProductCategory = new ProductCategory(1, "test", "test");
         List<ProductTag> testProductTags = new List<ProductTag>();
         Product auctionProduct, buyProduct;
+        List<Image> images = new List<Image>();
 
         [SetUp]
         public void Setup()
@@ -34,11 +36,14 @@ namespace MarketMinds.Tests.AuctionProductsRepositoryTest
 
             connection = new DataBaseConnection(config);
             auctionProductRepository = new AuctionProductsRepository(connection);
-                
+
             testDbHelper.PrepareTestDatabase();
 
-            auctionProduct = new AuctionProduct(1, "Test Product", "Test Description", testSeller, testProductCondition, testProductCategory, testProductTags, new List<Image>(), new DateTime(2025, 04, 04), new DateTime(2030, 04, 04), 100);
-            buyProduct = new BuyProduct(1, "Test Product", "Test Description", testSeller, testProductCondition, testProductCategory, testProductTags, new List<Image>(), 100);
+            testProductTags.Add(new ProductTag(1, "testtag1"));
+            testProductTags.Add(new ProductTag(2, "testtag2"));
+            images.Add(new Image("https://i.imgur.com/YYXgjHM.jpeg"));
+            auctionProduct = new AuctionProduct(1, "Test Product", "Test Description", testSeller, testProductCondition, testProductCategory, testProductTags, images, new DateTime(2025, 04, 04), new DateTime(2030, 04, 04), 100);
+            buyProduct = new BuyProduct(1, "Test Product", "Test Description", testSeller, testProductCondition, testProductCategory, testProductTags, images, 100);
         }
 
         [Test]
@@ -48,6 +53,76 @@ namespace MarketMinds.Tests.AuctionProductsRepositoryTest
 
             Assert.That(products, Is.Not.Null);
             Assert.That(products.GetType(), Is.EqualTo(typeof(List<Product>)));
+        }
+
+        [Test]
+        public void TestGetProducts_ProcessesDataTableRows_CreatesAuctionProducts()
+        {
+            auctionProductRepository.AddProduct(auctionProduct);
+
+            var products = auctionProductRepository.GetProducts();
+
+            Assert.That(products, Is.Not.Null);
+            Assert.That(products.Count, Is.GreaterThan(0));
+
+            var retrievedProduct = products.FirstOrDefault(p => p.Id == auctionProduct.Id) as AuctionProduct;
+            Assert.That(retrievedProduct, Is.Not.Null);
+            Assert.That(retrievedProduct.Title, Is.EqualTo(auctionProduct.Title));
+            Assert.That(retrievedProduct.Description, Is.EqualTo(auctionProduct.Description));
+            Assert.That(retrievedProduct.Seller.Id, Is.EqualTo(auctionProduct.Seller.Id));
+            Assert.That(retrievedProduct.Condition.Id, Is.EqualTo(auctionProduct.Condition.Id));
+            Assert.That(retrievedProduct.Category.Id, Is.EqualTo(auctionProduct.Category.Id));
+        }
+
+        [Test]
+        public void TestGetProducts_CorrectlyConvertsDataTypes()
+        {
+            auctionProductRepository.AddProduct(auctionProduct);
+
+            var products = auctionProductRepository.GetProducts();
+            var retrievedProduct = products.FirstOrDefault(p => p.Id == auctionProduct.Id) as AuctionProduct;
+
+            Assert.That(retrievedProduct, Is.Not.Null);
+
+            var auctionProductCasted = auctionProduct as AuctionProduct;
+            Assert.That(retrievedProduct.StartingPrice, Is.EqualTo(auctionProductCasted.StartingPrice));
+            Assert.That(retrievedProduct.StartingPrice, Is.TypeOf<float>());
+
+            Assert.That(retrievedProduct.StartAuctionDate, Is.EqualTo(auctionProductCasted.StartAuctionDate));
+            Assert.That(retrievedProduct.EndAuctionDate, Is.EqualTo(auctionProductCasted.EndAuctionDate));
+        }
+
+        [Test]
+        public void TestGetProducts_WithMultipleProducts_ReturnsAllProducts()
+        {
+            var secondProduct = new AuctionProduct(
+                2,
+                "Second Auction",
+                "Another Description",
+                testSeller,
+                testProductCondition,
+                testProductCategory,
+                testProductTags,
+                images,
+                new DateTime(2025, 05, 05),
+                new DateTime(2030, 05, 05),
+                200);
+
+            auctionProductRepository.AddProduct(auctionProduct);
+            auctionProductRepository.AddProduct(secondProduct);
+
+            var products = auctionProductRepository.GetProducts();
+
+            Assert.That(products.Count, Is.GreaterThanOrEqualTo(2));
+
+            var firstRetrieved = products.FirstOrDefault(p => p.Id == auctionProduct.Id);
+            var secondRetrieved = products.FirstOrDefault(p => p.Id == secondProduct.Id);
+
+            Assert.That(firstRetrieved, Is.Not.Null);
+            Assert.That(secondRetrieved, Is.Not.Null);
+
+            Assert.That(firstRetrieved.Title, Is.EqualTo(auctionProduct.Title));
+            Assert.That(secondRetrieved.Title, Is.EqualTo(secondProduct.Title));
         }
 
         [Test]
@@ -72,6 +147,28 @@ namespace MarketMinds.Tests.AuctionProductsRepositoryTest
 
             Assert.That(retrievedProduct, Is.Not.Null);
             Assert.That(retrievedProduct.Id, Is.EqualTo(productId));
+        }
+
+        [Test]
+        public void TestIProductsRepositoryGetProductByID_ForwardsToGetProductByID()
+        {
+            auctionProductRepository.AddProduct(auctionProduct);
+
+            IProductsRepository productsRepository = auctionProductRepository;
+
+            int productId = 1;
+            var retrievedProduct = productsRepository.GetProductByID(productId);
+
+            var directRetrievedProduct = auctionProductRepository.GetProductByID(productId);
+
+            Assert.That(retrievedProduct, Is.Not.Null);
+            Assert.That(retrievedProduct.Id, Is.EqualTo(productId));
+
+            Assert.That(retrievedProduct.Id, Is.EqualTo(directRetrievedProduct.Id));
+            Assert.That(retrievedProduct.Title, Is.EqualTo(directRetrievedProduct.Title));
+            Assert.That(retrievedProduct.Description, Is.EqualTo(directRetrievedProduct.Description));
+
+            Assert.That(retrievedProduct, Is.InstanceOf<AuctionProduct>());
         }
 
         [Test]
@@ -111,7 +208,6 @@ namespace MarketMinds.Tests.AuctionProductsRepositoryTest
         [Test]
         public void TestDeleteProduct_ValidProduct_DeletesProduct()
         {
-
             Assert.DoesNotThrow(() => auctionProductRepository.DeleteProduct(auctionProduct));
 
             var retrievedProduct = auctionProductRepository.GetProductByID(auctionProduct.Id);
--- MarketMinds.Test/Repositories/BasketRepositoryTest/BasketRepositoryTest.cs
+++ MarketMinds.Test/Repositories/BasketRepositoryTest/BasketRepositoryTest.cs
@@ -38,9 +38,11 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
             buyProductRepository = new BuyProductsRepository(connection);
 
             testDbHelper.PrepareTestDatabase();
-             
+
             testProduct = new BuyProduct(1, "Test Product", "Test Description", new User(2, "test seller", "test seller email"), new ProductCondition(1, "New", "New"), new ProductCategory(1, "Electronics", "Electic"), new List<ProductTag>(), new List<Image>(), 100);
-            testBasket = new Basket(1);
+            buyProductRepository.AddProduct(testProduct);
+
+            testBasket = basketRepository.GetBasketByUserId(testUser.Id);
         }
 
         [Test]
@@ -62,6 +64,96 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
         }
 
         [Test]
+        public void TestGetBasketByUserId_CreateBasketForNewUser_ReturnsBasket()
+        {
+            int existingUserId = 2;
+
+            var basket = basketRepository.GetBasketByUserId(existingUserId);
+
+            Assert.That(basket, Is.Not.Null);
+            Assert.That(basket.Id, Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void TestGetBasketItems_BasketHasTagsAndImages_ReturnsItemsWithTagsAndImages()
+        {
+            testBasket = basketRepository.GetBasketByUserId(testUser.Id);
+            int basketId = testBasket.Id;
+
+
+            var productTag = new ProductTag(1, "Test Tag");
+            var productImage = new Image("test-image-url.jpg");
+
+            string uniqueTitle = "Product With Tags And Images " + Guid.NewGuid().ToString().Substring(0, 8);
+            var productWithTagsAndImages = new BuyProduct(
+                0,
+                uniqueTitle,
+                "Test product that has both tags and images",
+                new User(2, "test seller", "test@example.com"),
+                new ProductCondition(1, "New", "New condition"),
+                new ProductCategory(1, "Electronics", "Electronic devices"),
+                new List<ProductTag> { productTag },
+                new List<Image> { productImage },
+                199.99f);
+
+            buyProductRepository.AddProduct(productWithTagsAndImages);
+
+            var products = buyProductRepository.GetProducts();
+            var addedProduct = products.FirstOrDefault(p => p.Title == uniqueTitle);
+
+
+            Assert.That(addedProduct, Is.Not.Null, "Product was not found after adding to repository");
+            Assert.That(addedProduct.Id, Is.GreaterThan(0), "Product does not have a valid ID");
+
+
+            int productId = addedProduct.Id;
+
+            basketRepository.AddItemToBasket(basketId, productId, 1);
+
+
+            var items = basketRepository.GetBasketItems(basketId);
+
+            Assert.That(items, Is.Not.Null);
+            Assert.That(items.Count, Is.GreaterThan(0));
+
+            var itemWithTagsAndImages = items.FirstOrDefault(i =>
+                i.Product.Title == uniqueTitle);
+
+            Assert.That(itemWithTagsAndImages, Is.Not.Null, "Could not find the product with tags and images");
+
+            Assert.That(itemWithTagsAndImages.Product.Tags, Is.Not.Null);
+            Assert.That(itemWithTagsAndImages.Product.Tags.Count, Is.GreaterThan(0));
+            var firstTag = itemWithTagsAndImages.Product.Tags[0];
+            Assert.That(firstTag, Is.Not.Null);
+
+            Assert.That(itemWithTagsAndImages.Product.Images, Is.Not.Null);
+            Assert.That(itemWithTagsAndImages.Product.Images.Count, Is.GreaterThan(0));
+            Assert.That(itemWithTagsAndImages.Product.Images[0].Url, Is.EqualTo("test-image-url.jpg"));
+        }
+
+        [Test]
+        public void TestGetBasketByUserId_PopulatesBasketWithItems()
+        {
+            testBasket = basketRepository.GetBasketByUserId(testUser.Id);
+            int basketId = testBasket.Id;
+
+            basketRepository.AddItemToBasket(basketId, testProduct.Id, 3);
+
+            Basket retrievedBasket = basketRepository.GetBasketByUserId(testUser.Id);
+
+            List<BasketItem> basketItems = retrievedBasket.GetItems();
+
+            Assert.That(basketItems, Is.Not.Null);
+            Assert.That(basketItems.Count, Is.GreaterThan(0));
+
+            BasketItem addedItem = basketItems.FirstOrDefault(item => item.Product.Id == testProduct.Id);
+
+            Assert.That(addedItem, Is.Not.Null, "Item was not found in the basket's internal collection");
+            Assert.That(addedItem.Quantity, Is.EqualTo(3), "Item quantity doesn't match expected value");
+            Assert.That(addedItem.Product.Title, Is.EqualTo(testProduct.Title), "Product title doesn't match");
+        }
+
+        [Test]
         public void TestAddItemToBasket_InvalidProduct_ThrowsException()
         {
             int basketId = testBasket.Id;
@@ -76,8 +168,6 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
         {
             testBasket = basketRepository.GetBasketByUserId(testUser.Id);
 
-            buyProductRepository.AddProduct(testProduct);
-
             int basketId = testBasket.Id;
             int productId = testProduct.Id;
             int quantity = 2;
@@ -87,6 +177,45 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
             List<BasketItem> items = basketRepository.GetBasketItems(basketId);
             Assert.That(items, Is.Not.Null);
             Assert.That(items.Any(i => i.Product.Id == productId && i.Quantity == quantity), Is.True);
+        }
+
+        [Test]
+        public void TestAddItemToBasket_NonExistentProduct_ThrowsException()
+        {
+            testBasket = basketRepository.GetBasketByUserId(testUser.Id);
+            int basketId = testBasket.Id;
+            int nonExistentProductId = 99999;
+            int quantity = 1;
+
+            Assert.Throws<Exception>(() => basketRepository.AddItemToBasket(basketId, nonExistentProductId, quantity));
+        }
+
+        [Test]
+        public void TestAddItemToBasket_InvalidQuantity_ThrowsException()
+        {
+            testBasket = basketRepository.GetBasketByUserId(testUser.Id);
+            int basketId = testBasket.Id;
+            int productId = testProduct.Id;
+            int invalidQuantity = -1;
+            Assert.Throws<ArgumentException>(() => basketRepository.AddItemToBasket(basketId, productId, invalidQuantity));
+        }
+
+        [Test]
+        public void TestAddItemToBasket_ExistingProduct_UpdatesQuantity()
+        {
+            testBasket = basketRepository.GetBasketByUserId(testUser.Id);
+            int basketId = testBasket.Id;
+            int productId = testProduct.Id;
+
+            basketRepository.AddItemToBasket(basketId, productId, 2);
+
+            basketRepository.AddItemToBasket(basketId, productId, 3);
+
+            List<BasketItem> items = basketRepository.GetBasketItems(basketId);
+            var item = items.FirstOrDefault(i => i.Product.Id == productId);
+
+            Assert.That(item, Is.Not.Null);
+            Assert.That(item.Quantity, Is.EqualTo(5));
         }
 
         [Test]
@@ -122,44 +251,17 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
         {
             testBasket = basketRepository.GetBasketByUserId(testUser.Id);
             int basketId = testBasket.Id;
+            int productId = testProduct.Id;
 
-            var product1 = new BuyProduct(
-                1,
-                "Test Product 1",
-                "Test Description",
-                new User(2, "test seller", "test seller email"),
-                new ProductCondition(1, "New", "New"),
-                new ProductCategory(1, "Electronics", "Electric"),
-                new List<ProductTag>(),
-                new List<Image>(),
-                100);
+            basketRepository.AddItemToBasket(basketId, productId, 1);
 
-            var product2 = new BuyProduct(
-                2,
-                "Test Product 2",
-                "Test Description",
-                new User(2, "test seller", "test seller email"),
-                new ProductCondition(1, "New", "New"),
-                new ProductCategory(1, "Electronics", "Electric"),
-                new List<ProductTag>(),
-                new List<Image>(),
-                200);
-
-            buyProductRepository.AddProduct(product1);
-            buyProductRepository.AddProduct(product2);
-
-            basketRepository.AddItemToBasket(basketId, product1.Id, 1);
-            basketRepository.AddItemToBasket(basketId, product2.Id, 1);
-
-            int productIdToUpdate = product1.Id;
             int newQuantity = 3;
-            basketRepository.UpdateItemQuantityByProductId(basketId, productIdToUpdate, newQuantity);
+            basketRepository.UpdateItemQuantityByProductId(basketId, productId, newQuantity);
 
             List<BasketItem> items = basketRepository.GetBasketItems(basketId);
             Assert.That(items, Is.Not.Null);
-            Assert.That(items.Count, Is.EqualTo(2), "Basket should contain 2 different products");
 
-            BasketItem updatedItem = items.FirstOrDefault(i => i.Product.Id == productIdToUpdate);
+            BasketItem updatedItem = items.FirstOrDefault(i => i.Product.Id == productId);
             Assert.That(updatedItem, Is.Not.Null);
             Assert.That(updatedItem.Quantity, Is.EqualTo(newQuantity));
         }
@@ -174,6 +276,16 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
         }
 
         [Test]
+        public void TestUpdateItemQuantityByProductId_InvalidBasketId_ThrowsException()
+        {
+            int invalidBasketId = -1;
+            int productId = testProduct.Id;
+            int quantity = 5;
+
+            Assert.Throws<ArgumentException>(() => basketRepository.UpdateItemQuantityByProductId(invalidBasketId, productId, quantity));
+        }
+
+        [Test]
         public void TestClearBasket_InvalidBasketId_ThrowsException()
         {
             int invalidBasketId = -1;
@@ -183,11 +295,13 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
         [Test]
         public void TestClearBasket_ValidBasketId_ClearsBasket()
         {
-            int basketId = 1;
+            int basketId = testBasket.Id;
+
+            basketRepository.AddItemToBasket(basketId, testProduct.Id, 1);
 
             basketRepository.ClearBasket(basketId);
-            List<BasketItem> items = basketRepository.GetBasketItems(basketId);
 
+            List<BasketItem> items = basketRepository.GetBasketItems(basketId);
             Assert.That(items, Is.Not.Null);
             Assert.That(items.Count, Is.EqualTo(0));
         }
@@ -208,11 +322,49 @@ namespace MarketMinds.Test.Repositories.BasketRepositoryTest
             int basketId = testBasket.Id;
             int productId = testProduct.Id;
 
+            basketRepository.AddItemToBasket(basketId, productId, 1);
+
             basketRepository.RemoveItemByProductId(basketId, productId);
 
             List<BasketItem> items = basketRepository.GetBasketItems(basketId);
             Assert.That(items, Is.Not.Null);
             Assert.That(items.Any(i => i.Product.Id == productId), Is.False);
+        }
+
+        [Test]
+        public void TestGetBasketItems_LoadsProductsWithDataFromBasket()
+        {
+            testBasket = basketRepository.GetBasketByUserId(testUser.Id);
+            int basketId = testBasket.Id;
+
+            basketRepository.AddItemToBasket(basketId, testProduct.Id, 3);
+
+            var items = basketRepository.GetBasketItems(basketId);
+
+            Assert.That(items, Is.Not.Null);
+            Assert.That(items.Count, Is.EqualTo(1));
+
+            var basketItem = items[0];
+
+            Assert.That(basketItem.Product, Is.Not.Null);
+            Assert.That(basketItem.Product.Id, Is.EqualTo(testProduct.Id));
+            Assert.That(basketItem.Product.Title, Is.EqualTo(testProduct.Title));
+            Assert.That(basketItem.Quantity, Is.EqualTo(3));
+
+            int initialItemCount = 0;
+
+            foreach (var item in items)
+            {
+                testBasket.AddItem(item.Product, item.Quantity);
+            }
+
+            var updatedItems = basketRepository.GetBasketItems(basketId);
+            Assert.That(updatedItems, Is.Not.Null);
+            Assert.That(updatedItems.Count, Is.GreaterThan(initialItemCount));
+
+            var addedItem = updatedItems.FirstOrDefault(i => i.Product.Id == testProduct.Id);
+            Assert.That(addedItem, Is.Not.Null);
+            Assert.That(addedItem.Quantity, Is.EqualTo(3));
         }
     }
 }
