68b28b2aa49b4373eb884444092303eb3f7d52bf
luca <lucac375@gmail.com>
2025-04-07 20:28:27 +0300

    more-tests

---
+++ MarketMinds.Test/Services/ListingCreationService/ListingCreationServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using DomainLayer.Domain;
+using MarketMinds.Services;
+using MarketMinds.Services.AuctionProductsService;
+using MarketMinds.Services.BorrowProductsService;
+using MarketMinds.Services.BuyProductsService;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace MarketMinds.Test.Services
+{
+    [TestFixture]
+    public class ListingCreationServiceTest
+    {
+        private Mock<IBuyProductsService> _mockBuyService;
+        private Mock<IBorrowProductsService> _mockBorrowService;
+        private Mock<IAuctionProductsService> _mockAuctionService;
+        private ListingCreationService _service;
+        private Product _testProduct;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockBuyService = new Mock<IBuyProductsService>();
+            _mockBorrowService = new Mock<IBorrowProductsService>();
+            _mockAuctionService = new Mock<IAuctionProductsService>();
+
+            _service = new ListingCreationService(
+                _mockBuyService.Object,
+                _mockBorrowService.Object,
+                _mockAuctionService.Object);
+        }
+
+        [Test]
+        public void CreateMarketListing_BuyType_CallsBuyProductsService()
+        {
+            _service.CreateMarketListing(_testProduct, "buy");
+
+            _mockBuyService.Verify(s => s.CreateListing(_testProduct), Times.Once);
+            _mockBorrowService.Verify(s => s.CreateListing(It.IsAny<Product>()), Times.Never);
+            _mockAuctionService.Verify(s => s.CreateListing(It.IsAny<Product>()), Times.Never);
+        }
+
+        [Test]
+        public void CreateMarketListing_BorrowType_CallsBorrowProductsService()
+        {
+            _service.CreateMarketListing(_testProduct, "borrow");
+
+            _mockBuyService.Verify(s => s.CreateListing(It.IsAny<Product>()), Times.Never);
+            _mockBorrowService.Verify(s => s.CreateListing(_testProduct), Times.Once);
+            _mockAuctionService.Verify(s => s.CreateListing(It.IsAny<Product>()), Times.Never);
+        }
+
+        [Test]
+        public void CreateMarketListing_AuctionType_CallsAuctionProductsService()
+        {
+            _service.CreateMarketListing(_testProduct, "auction");
+
+            _mockBuyService.Verify(s => s.CreateListing(It.IsAny<Product>()), Times.Never);
+            _mockBorrowService.Verify(s => s.CreateListing(It.IsAny<Product>()), Times.Never);
+            _mockAuctionService.Verify(s => s.CreateListing(_testProduct), Times.Once);
+        }
+
+        [Test]
+        public void CreateMarketListing_InvalidType_ThrowsArgumentException()
+        {
+            var exception = Assert.Throws<ArgumentException>(() =>
+                _service.CreateMarketListing(_testProduct, "invalid_type"));
+
+            Assert.That(exception.Message, Does.Contain("Invalid listing type"));
+        }
+
+        [Test]
+        public void CreateMarketListing_CaseInsensitive_WorksCorrectly()
+        {
+            _service.CreateMarketListing(_testProduct, "BUY");
+
+            _mockBuyService.Verify(s => s.CreateListing(_testProduct), Times.Once);
+        }
+    }
+}
---
+++ MarketMinds.Test/Services/SortTypeConverterService/SortTypeConverterServiceTest.cs
@@ -0,0 +1,69 @@
+﻿using DomainLayer.Domain;
+using MarketMinds.Services;
+using NUnit.Framework;
+
+namespace MarketMinds.Test.Services
+{
+    [TestFixture]
+    public class SortTypeConverterServiceTest
+    {
+        private SortTypeConverterService _converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _converter = new SortTypeConverterService();
+        }
+
+        [Test]
+        public void Convert_PriceAsc_ReturnsCorrectSortType()
+        {
+            string sortTag = "PriceAsc";
+            var result = _converter.Convert(sortTag);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.ExternalAttributeFieldTitle, Is.EqualTo("Price"));
+            Assert.That(result.InternalAttributeFieldTitle, Is.EqualTo("Price"));
+            Assert.That(result.IsAscending, Is.True);
+        }
+
+        [Test]
+        public void Convert_PriceDesc_ReturnsCorrectSortType()
+        {
+            string sortTag = "PriceDesc";
+            var result = _converter.Convert(sortTag);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.ExternalAttributeFieldTitle, Is.EqualTo("Price"));
+            Assert.That(result.InternalAttributeFieldTitle, Is.EqualTo("Price"));
+            Assert.That(result.IsAscending, Is.False);
+        }
+
+        [Test]
+        public void Convert_InvalidSortTag_ReturnsNull()
+        {
+            string sortTag = "InvalidSortTag";
+            var result = _converter.Convert(sortTag);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Convert_EmptyString_ReturnsNull()
+        {
+            string sortTag = "";
+            var result = _converter.Convert(sortTag);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Convert_NullString_ReturnsNull()
+        {
+            string sortTag = null;
+            var result = _converter.Convert(sortTag);
+
+            Assert.That(result, Is.Null);
+        }
+    }
+}
